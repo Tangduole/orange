bf1f79745233138b0887df7da8387fb5d019b8f6
xiaodianlv <xiaodianlv@example.com>
2026-04-06 22:19:35 +0800

    chore: 更新电容配置，添加GallerySaver插件支持

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.orange.downloader',
   appName: 'Orange Downloader',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    GallerySaver: {},
+  },
 };
 
 export default config;
